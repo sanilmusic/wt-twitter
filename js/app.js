988ee1ad5db46760b4111812d789eae863f80be0
Sanil Musić <music.sanil@gmail.com>
2016-11-19 13:42:26 +0100

    Refactoring

--- js/app.js
+++ js/app.js
@@ -15,75 +15,103 @@ function ajaxRequest(method, url, callback) {
     request.send();
 }
 
-function ucitajStranicu(ime) {
-    loadingAnimation('show');
-    ajaxRequest('GET', 'partials/' + ime + '.html', function(html) {
-        document.getElementById('sadrzaj').innerHTML = html;
-        loadingAnimation('hide');
-    });
-}
+TWITTER = {
+    common: function() {
+        var obavijesti = document.getElementById('obavijesti');
 
-function parsirajUrl() {
-    if (location.hash === '' || location.hash === '#') {
-        ucitajStranicu('index');
-    } else {
-        ucitajStranicu(location.hash.substr(1));
-    }
-}
-
-window.onhashchange = parsirajUrl;
-window.onload = function() {
-    parsirajUrl();
-
-    document.getElementsByTagName('body').item(0).addEventListener('click', function(e) {
-        var el = e.target;
-        while (el !== null && el.tagName !== 'A') {
-            el = el.parentElement;
-        }
-        if (el === null) {
-            return;
-        } else if (el.dataset.traverse === 'false') {
-            e.preventDefault();
+        if (obavijesti) {
+            obavijesti.addEventListener('click', function(e) {
+                e.preventDefault();
+            });
         }
 
         // Meni
-        if (el.id === 'toggle-menu') {
-            var meni = document.getElementById('max-640-meni');
-            meni.style.display = (meni.style.display === 'block' ? 'none' : 'block');
-        }
+        var toggle = document.getElementById('toggle-menu');
 
-        // Galerija
-        if (el.className === 'prethodna' || el.className === 'naredna') {
-            var roditelj = document.getElementById('galerija'),
-                aktivna = roditelj.getElementsByClassName('aktivna').item(0),
-                nova = null;
-            
-            if (el.className === 'prethodna') {
+        if (toggle) {
+            toggle.addEventListener('click', function(e) {
+                e.preventDefault();
+
+                var meni = document.getElementById('max-640-meni');
+                meni.style.display = (meni.style.display === 'block' ? 'none' : 'block');
+            });
+        }
+    },
+    profil: function() {
+        var roditelj = document.getElementById('galerija');
+
+        roditelj.getElementsByClassName('prethodna').item(0).addEventListener('click', function(e) {
+            e.preventDefault();
+
+            var aktivna = roditelj.getElementsByClassName('aktivna').item(0),
                 nova = aktivna.previousElementSibling;
 
-                if (nova === null || nova.tagName !== 'IMG') {
-                    // Pomjeramo se do posljednje slike
-                    nova = aktivna;
-
-                    while (nova.nextElementSibling && nova.nextElementSibling.tagName === 'IMG') {
-                        nova = nova.nextElementSibling;
-                    }
-                }
-            } else if (el.className === 'naredna') {
-                nova = aktivna.nextElementSibling;
-
-                if (nova === null || nova.tagName !== 'IMG') {
-                    // Pomjeramo se do prve slike
-                    nova = aktivna;
-
-                    while (nova.previousElementSibling && nova.previousElementSibling.tagName === 'IMG') {
-                        nova = nova.previousElementSibling;
-                    }
+            if (!nova || nova.tagName !== 'IMG') {
+                // Pomjeramo se do posljednje slike
+                nova = aktivna;
+                while (nova.nextElementSibling && nova.nextElementSibling.tagName === 'IMG') {
+                    nova = nova.nextElementSibling;
                 }
             }
 
             aktivna.className = '';
             nova.className = 'aktivna';
+        });
+
+        roditelj.getElementsByClassName('naredna').item(0).addEventListener('click', function(e) {
+            e.preventDefault();
+
+            var aktivna = roditelj.getElementsByClassName('aktivna').item(0),
+                nova = aktivna.nextElementSibling;
+
+            if (!nova || nova.tagName !== 'IMG') {
+                // Pomjeramo se do prve slike
+                nova = aktivna;
+                while (nova.previousElementSibling && nova.previousElementSibling.tagName === 'IMG') {
+                    nova = nova.previousElementSibling;
+                }
+            }
+
+            aktivna.className = '';
+            nova.className = 'aktivna';
+        })
+    }
+};
+
+UTIL = {
+    exec: function(sta) {
+        var ns = TWITTER;
+
+        if (ns['common'] && typeof ns['common'] === 'function') {
+            ns['common']();
         }
+
+        if (sta !== '' && ns[sta] && typeof ns[sta] === 'function') {
+            ns[sta]();
+        }
+    },
+};
+
+function ucitajStranicu(ime) {
+    loadingAnimation('show');
+    ajaxRequest('GET', 'partials/' + ime + '.html', function(html) {
+        document.getElementById('sadrzaj').innerHTML = html;
+        loadingAnimation('hide');
+        UTIL.exec(ime);
     });
 }
+
+function parsirajUrl() {
+    var hash = null;
+
+    if (location.hash === '' || location.hash === '#') {
+        hash = 'index';
+    } else {
+        hash = location.hash.substr(1);
+    }
+
+    ucitajStranicu(hash);
+}
+
+window.onhashchange = parsirajUrl;
+window.onload = parsirajUrl;
