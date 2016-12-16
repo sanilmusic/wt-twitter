a7f9bd5076e6abd443a1bf7cb335f759a0cce951
Sanil Musić <music.sanil@gmail.com>
2016-12-16 19:18:00 +0100

    Sitni refactoring

---
+++ app/Views/Partials/posljednjePoruke.php
@@ -0,0 +1,15 @@
+<h3>Posljednje poruke</h3>
+<div class="posljednje-poruke">
+    <div class="clearfix poruka">
+        <img class="slika-profila" src="images/user.png" alt="Korisnik">
+        <div class="balon">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est justo, fringilla sed ornare quis, consectetur vel odio. Sed purus risus, porta a orci non, elementum accumsan nisi. Duis ultrices aliquet hendrerit. Aliquam erat volutpat.
+        </div>
+    </div>
+    <div class="clearfix poruka">
+        <img class="slika-profila" src="images/user.png" alt="Korisnik">
+        <div class="balon">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est justo, fringilla sed ornare quis, consectetur vel odio. Sed purus risus, porta a orci non, elementum accumsan nisi. Duis ultrices aliquet hendrerit. Aliquam erat volutpat.
+        </div>
+    </div>
+</div>
--- app/Views/Pocetni/index.php
+++ app/Views/Pocetni/index.php
@@ -3,21 +3,7 @@
     <div class="kontejner">
         <div class="red">
             <div class="kolona kolona-3">
-                <h3>Posljednje poruke</h3>
-                <div class="posljednje-poruke">
-                    <div class="clearfix poruka">
-                        <img class="slika-profila" src="images/user.png" alt="Korisnik">
-                        <div class="balon">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est justo, fringilla sed ornare quis, consectetur vel odio. Sed purus risus, porta a orci non, elementum accumsan nisi. Duis ultrices aliquet hendrerit. Aliquam erat volutpat.
-                        </div>
-                    </div>
-                    <div class="clearfix poruka">
-                        <img class="slika-profila" src="images/user.png" alt="Korisnik">
-                        <div class="balon">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est justo, fringilla sed ornare quis, consectetur vel odio. Sed purus risus, porta a orci non, elementum accumsan nisi. Duis ultrices aliquet hendrerit. Aliquam erat volutpat.
-                        </div>
-                    </div>
-                </div>
+                <?php view('Partials/posljednjePoruke') ?>
             </div>
             <div class="kolona kolona-3">
                 <h3>Novi nalog?</h3>
