78cb310de6d40ce37412ed77dcd90479289e6853
Sanil Musić <music.sanil@gmail.com>
2016-12-28 10:47:21 +0100

    Greškica

--- app/Views/Partials/Admin/meni.php
+++ app/Views/Partials/Admin/meni.php
@@ -1,7 +1,7 @@
 <div class="red">
     <div class="kolona kolona-6 admin-meni">
         <ul>
-            <li><a href="oindex.php?sta=admin/korisnici">Korisnici</a></li>
+            <li><a href="index.php?sta=admin/korisnici">Korisnici</a></li>
             <li><a href="index.php?sta=admin/poruke">Poruke</a></li>
         </ul>
     </div>
